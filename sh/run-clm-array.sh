faa3bb942ed9785d46a74b7707c318a23c1648b2
Seungchan An <ann081993@gmail.com>
2026-02-12 23:36:51 -0500

    add scripts to submit slurm jobs - array job submitter + launcher

---
+++ sh/run-clm-array.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --job-name=clm
+#SBATCH --partition=main,skinniderlab
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --time=96:00:00
+#SBATCH --output=logs/hub_%A_%a.log
+#SBATCH --error=logs/hub_%A_%a.log
+
+
+set -euo pipefail
+cd ~/git/CLM
+
+# -------------------------
+# conda init
+# -------------------------
+__conda_setup="$('/Genomics/skinniderlab/ms0270/miniconda3/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    source /Genomics/skinniderlab/ms0270/miniconda3/etc/profile.d/conda.sh
+fi
+unset __conda_setup
+
+conda activate clm
+
+echo "hub job started on $(hostname)"
+date
+
+
+# -------------------------
+# grid param selection
+# -------------------------
+GRID_CONFIG=$(sed -n "$((SLURM_ARRAY_TASK_ID+1))p" "$GRID_FILE")
+
+if [ -z "$GRID_CONFIG" ]; then
+    echo "no grid config file found"
+    exit 1
+fi
+
+echo "array id: $SLURM_ARRAY_TASK_ID"
+echo "using config: $GRID_CONFIG"
+
+
+# -------------------------
+# run snakemake hub
+# -------------------------
+snakemake all \
+  --snakefile workflow/Snakefile \
+  --configfile "$GRID_CONFIG" \
+  --jobs 128 \
+  --slurm --default-resources slurm_partition=main,skinniderlab \
+  --latency-wait 60 \
+  --rerun-incomplete \
+  --keep-going \
+  -p
+
+echo "hub job finished"
+date
---
+++ sh/submit-clm-array.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# example:
+# bash sh/submit-clm-array.sh sh/grids/grid_bsm_AL.txt
+
+set -euo pipefail
+cd ~/git/CLM
+
+GRID_FILE=${1:?provide grid file}
+
+
+N=$(wc -l < "$GRID_FILE")
+MAX=$((N-1))
+
+echo "submitting array 0-$MAX from $GRID_FILE"
+
+sbatch \
+  --array=0-$MAX \
+  --export=ALL,GRID_FILE="$GRID_FILE" \
+  sh/run-clm-array.sh
